c4461e3ffa8b05956e11e253d84410dd057e5444
daichi-code <daichi.pr.daichi@gmail.com>
2020-10-14 17:04:04 +0900

    activehashを用いた指定の値の登録

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :day
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping
+  belongs_to_active_hash :prefecture
+
   belongs_to :user
 
   has_one_attached :image
@@ -12,6 +19,13 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :day_id, presence: true
   validates :image, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :day_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_id, numericality: { other_than: 1 }
+  validates :prefecure_id, numericality: { other_than: 1 }
+
 
   validates :price, :numericality => { :greater_than_or_equal_to => 300 }
   validates :price, :numericality => { :less_than_or_equal_to => 9999999 }
