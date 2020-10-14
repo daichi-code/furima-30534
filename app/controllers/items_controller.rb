82ca6841e54c84bba7f29c86df9c21eb62b8a1b9
daichi-code <daichi.pr.daichi@gmail.com>
2020-10-14 18:00:47 +0900

    商品の保存機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -22,7 +27,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:title, :text, :category_id, :price, :condition_id, :shipping_id, :prefecture_id, :day_id, :image).merge(user_id: current_user.prefecture_id)
+    params.require(:item).permit(:title, :text, :category_id, :price, :condition_id, :shipping_id, :prefecture_id, :day_id, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
--- app/models/item.rb
+++ app/models/item.rb
@@ -24,7 +24,7 @@ class Item < ApplicationRecord
   validates :day_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
   validates :shipping_id, numericality: { other_than: 1 }
-  validates :prefecure_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
 
 
   validates :price, :numericality => { :greater_than_or_equal_to => 300 }
