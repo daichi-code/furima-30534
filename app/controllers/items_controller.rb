3609f57683aabfa6ff4b9ccc26b7110f310ccb78
daichi-code <daichi.pr.daichi@gmail.com>
2020-10-14 12:38:45 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all
@@ -10,15 +11,25 @@ class ItemsController < ApplicationController
   end
 
   def create
-    #@item = Item.new(item_params)
+    @item = Item.new(item_params)
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
 
 
 
   private
   def item_params
     params.require(:item).permit(:title, :text, :category_id, :price, :condition_id, :shipping_id, :prefecture_id, :day_id, :image).merge(user_id: current_user.prefecture_id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to action: :index
+    end
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,22 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :day_id, presence: true
+  validates :image, presence: true
+
+  validates :price, :numericality => { :greater_than_or_equal_to => 300 }
+  validates :price, :numericality => { :less_than_or_equal_to => 9999999 }
+
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price
+  end
 end
