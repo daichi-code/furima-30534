6d10dd023141bdee08643c0e0993e4d4f4d63288
daichi-code <daichi.pr.daichi@gmail.com>
2020-10-15 12:16:21 +0900

    機能追加まで

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
+    title {"sample"}
+    text {"sample"}
+    category_id {"id2"}
+    condition_id {"id2"}
+    shipping_id {"id2"}
+    prefecture_id {"id2"}
+    day_id {"id2"}
     
   end
 end
