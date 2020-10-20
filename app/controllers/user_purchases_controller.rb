3497688a0ebf8cd12fd7a1f9de08041fe84bf444
daichi-code <daichi.pr.daichi@gmail.com>
2020-10-20 21:50:38 +0900

    商品購入機能まで

---
+++ app/controllers/user_purchases_controller.rb
@@ -0,0 +1,38 @@
+class UserPurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
+
+  def index
+    @order = UserPurchases.new
+  end
+
+  def create
+    @order = UserPurchases.new(purchase_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:user_purchases).permit(:token, :postal_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+end
---
+++ app/models/user_purchases.rb
@@ -0,0 +1,21 @@
+class UserPurchases
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :phone_number, :building_name, :token , :user_id, :item_id
+
+
+  with_options presence: true do
+  validates :postal_code, format: {with: /\A^\d{3}[-]\d{4}$\z/ }
+  validates :municipalities
+  validates :address
+  validates :phone_number, format: {with: /\A^\d{10}$|^\d{11}$\z/ }
+  validates :token
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :prefecture_id, numericality: { other_than: 1 }
+
+  def save
+    @order = Order.create(user_id: user_id, item_id: item_id)
+    Purchase.create(order_id: @order.id, postal_code: postal_code, prefecture: prefecture_id, municipalities: municipalities, address: address, building_name: building_name, phone_number: phone_number)
+  end
+end
