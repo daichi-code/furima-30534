c928b2b087414d8544f164489ce513832e625218
daichi-code <daichi.pr.daichi@gmail.com>
2020-10-21 14:03:54 +0900

    商品購入機能の実装

--- app/controllers/user_purchases_controller.rb
+++ app/controllers/user_purchases_controller.rb
@@ -1,5 +1,6 @@
 class UserPurchasesController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_root_path, only: [:index]
 
 
   def index
@@ -35,4 +36,11 @@ class UserPurchasesController < ApplicationController
     )
   end
 
+  def move_to_root_path
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    elsif @item.order.present?
+      redirect_to root_path
+    end
+  end
 end
